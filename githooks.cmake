e0563dce4301867a15fff4a36e62ad6826aeb75a
Daniel Bruder <daniel.bruder+git@gmail.com>
2013-09-27 16:40:53 +0200

    first version

---
+++ githooks.cmake
@@ -0,0 +1,25 @@
+### Set the hook's name
+set(GIT_PRE_RELEASE_HOOK_FILE "${PROJECT_SOURCE_DIR}/.git/hooks/pre-release.sh")
+
+### Set the hook's content
+set(GIT_PRE_RELEASE_HOOK_FILE_CONTENT 
+"#!/bin/sh
+
+echo Put your foobar-hook-content here.
+echo (Take care of your backslashes)
+")
+
+if(EXISTS ${GIT_PRE_RELEASE_HOOK_FILE})
+    message("-- git pre-release hook exists")
+else()
+    file(WRITE
+        ${GIT_PRE_RELEASE_HOOK_FILE}
+        ${GIT_PRE_RELEASE_HOOK_FILE_CONTENT}
+    )
+endif()
+
+if(EXISTS ${GIT_PRE_RELEASE_HOOK_FILE})
+    message("-- git pre-release hook created.")
+else()
+    message("-- creating git hook failed.")
+endif()
